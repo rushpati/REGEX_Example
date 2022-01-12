6da020d8a874a3376f3f2f4db1fd70cd3f33212d
rushi <harshadpatil132@gmail.com>
2022-01-12 22:20:38 +0530

    some files added

--- src/Text.java
+++ src/Text.java
@@ -1,5 +1,25 @@
-public class Text extends Base
-{
+import java.util.LinkedList;
+import java.util.List;
 
+public class Text<H> {
+
+    private H val ;
+    public Text(H t){
+        this.val =t;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + val + "]";
+    }
+
+    public static void main(String[] args) {
+        List list = new LinkedList<>();
+        list.add("1");
+        list.add("1dfs");
+        list.add("1fs");
+        list.add("1235");
+
+    }
 
 }
